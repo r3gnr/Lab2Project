61492c27d1ad3867c2e68bc4c65ed3d94fda7e93
r3gnr <apatiregn@gmail.com>
2025-05-13 23:32:24 +0700

    Create Vacancy class

---
+++ Lab2Project/src/Vacancy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Vacancy
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> Skills { get; set; } = new List<string>();
+    public string Link { get; set; }
+    public double Relevance { get; set; }
+}
