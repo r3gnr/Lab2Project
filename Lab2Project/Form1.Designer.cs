d986d64fce3f7158da0a58b0b449fac53e228f34
r3gnr <apatiregn@gmail.com>
2025-05-14 01:51:46 +0700

    Add UI

--- Lab2Project/Form1.Designer.cs
+++ Lab2Project/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.AccessibleName = "SkillsTextBox";
+            this.textBox1.Size = new System.Drawing.Size(293, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 18);
+            this.button1.Name = "button1";
+            this.button1.AccessibleName = "SearchButton";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.AccessibleName = "VacanciesGrid";
+            this.dataGridView1.Size = new System.Drawing.Size(360, 334);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.AccessibleName = "SaveButton";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Lab2Project/Form1.cs
+++ Lab2Project/Form1.cs
@@ -1,20 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Linq;
 
 namespace Lab2Project
 {
     public partial class Form1 : Form
     {
+        private Ranker _ranker;
+        private List<Vacancy> _vacancies;
+
         public Form1()
         {
             InitializeComponent();
+            _ranker = new Ranker(new List<string>());
+            _vacancies = new List<Vacancy>();
+        }
+
+        // Обработчик кнопки "Поиск"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var skills = textBox1.Text.Split(',').Select(s => s.Trim()).ToList();
+
+                // Если навыки не введены, выбрасываем исключение
+                if (skills.Count == 0 || string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    throw new ArgumentException("Не указаны навыки для поиска");
+                }
+
+                _ranker = new Ranker(skills);
+                _vacancies = new MockDataCollector().GetVacancies();
+                _vacancies = _ranker.RankList(_vacancies);
+                dataGridView1.DataSource = _vacancies;
+                label1.Text = $"Статус: Найдено {_vacancies.Count} вакансий";
+            }
+            catch (Exception ex)
+            {
+                label1.Text = $"Статус: Ошибка - {ex.Message}";
+                dataGridView1.DataSource = null; // Очищаем таблицу
+            }
+        }
+
+        // Обработчик кнопки "Сохранить"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_vacancies == null || _vacancies.Count == 0)
+            {
+                MessageBox.Show("Нет данных для сохранения!");
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText("vacancies.txt", string.Join("\n", _vacancies.Select(v => v.Name)));
+                label1.Text = "Статус: Данные сохранены в vacancies.txt";
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Статус: Ошибка сохранения - " + ex.Message;
+            }
         }
     }
 }
